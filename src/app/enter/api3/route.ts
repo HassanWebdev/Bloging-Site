d7c7264bcaee099f7c02084f205a608fc1f24848
Muh@mm@d  H@ss@n R@z@ <162709809+HassanWebdev@users.noreply.github.com>
2024-06-11 17:15:11 +0500

    Update route.ts

--- src/app/enter/api3/route.ts
+++ src/app/enter/api3/route.ts
@@ -3,12 +3,11 @@ import { AuthList } from '@/lib/createSchema';
 import { NextResponse } from 'next/server';
 
 
-
-export async function POST(req: NextResponse) {
+export const POST = async (request: Request): Promise<NextResponse> => {
     interface User{
         email:string,
     }
-   try{ const body: User = await req.json()
+   try{ const body: User = await request.json()
     const { email } = body
     const database = process.env.MONGODB_URI || 'mongodb+srv://hassanwebdev0896:M.saim777@cluster0.a6knlop.mongodb.net/Next-js?retryWrites=true&w=majority&appName=Cluster0'
     await mongoose.connect(database);
