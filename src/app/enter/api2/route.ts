66fd7fa0a3b51bd6f6817af4ef4a5b02a0269ebf
Muh@mm@d  H@ss@n R@z@ <162709809+HassanWebdev@users.noreply.github.com>
2024-06-11 17:14:44 +0500

    Update route.ts

--- src/app/enter/api2/route.ts
+++ src/app/enter/api2/route.ts
@@ -7,7 +7,7 @@ export const POST = async (request: Request): Promise<NextResponse> => {
     interface User{
         email:string,
     }
-   try{ const body: User = await req.json()
+   try{ const body: User = await request.json()
     const { email } = body
     const database = process.env.MONGODB_URI || 'mongodb+srv://hassanwebdev0896:M.saim777@cluster0.a6knlop.mongodb.net/Next-js?retryWrites=true&w=majority&appName=Cluster0'
     await mongoose.connect(database);
