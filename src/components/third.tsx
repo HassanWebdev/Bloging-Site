daac69e3d81b07ad0336403d2c4f61c68896392d
H@ss@n R@z@ <162709809+HassanWebdev@users.noreply.github.com>
2024-06-25 10:32:05 +0500

    Update third.tsx

--- src/components/third.tsx
+++ src/components/third.tsx
@@ -49,7 +49,7 @@ const Third = () => {
                     />
                   </div>
                   <div className='w-full h-[35%] flex flex-col gap-4'>
-                    <h1 className='text-sm font-sans'>Jun 12, 2023 2 min</h1>
+                    <h1 className='text-sm font-sans'>item.date</h1>
                     <h1 className='break-words  font-title text-xl'>
                       {item.title}
                     </h1>
